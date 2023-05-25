47665967acc5875a0f37a7f26241185ded83326d
Raj <rchak045@gmail.com>
2023-05-25 13:15:38 -0400

    PLOTS added

--- test.py
+++ test.py
@@ -33,3 +33,29 @@ wave.burst_characteristics()
 scope.single()
 wave.trigger()
 wave.output(1)
+#                 PLOTTING SECTION
+import matplotlib.pyplot as plt
+
+times1, volts1 = scope.get_waveform_binary()
+times2, volts2 = scope.get_waveform_ascii(channel=2)
+
+fig, ax = plt.subplots()
+ax.plot(times2, volts2, label='VC')
+ax.plot(times1, volts1, label='Vin')
+ax.set_ylabel('Voltage')
+ax.set_xlabel('Time')
+ax.set_title('Pulse Wave')
+ax.legend()  # Add a legend based on the labels
+
+plt.savefig("Test.jpg")
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(times2, volts2, label='VC')
+ax.plot(times2, volts2, 'k:', label='Estimation')  # Dotted line for VC curve
+plt.savefig("CurveFit.jpg")
+plt.show()
+
+
+
+scope.screen_capture()
